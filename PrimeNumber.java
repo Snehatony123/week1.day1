08b0e4a29e2af521ff0bf248f66764df5977ade1
Snehatony123 <107301041+Snehatony123@users.noreply.github.com>
2022-06-13 23:50:21 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+  package week1.day1.assignments;
+  public class PrimeNumber {
+	 public static void main(String[] args) {
+	 int number=13;
+	 if((number/number==1)&&(number/1==number)){
+     System.out.println(number+" is a prime number");
+     }
+     else 
+     {
+	 System.out.println(number+" is Not a prime number");
+     }
+	 
+	
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+
+}
